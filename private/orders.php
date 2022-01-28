2b5739e360c11a70a1bd28e202636217976e0cc0
Enrico Pezzano <enrico@MacBook-Air-di-Enrico.local>
2022-01-28 11:02:02 +0100

    :(

--- private/orders.php
+++ private/orders.php
@@ -1,50 +1,42 @@
 <?php
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //error reporting for mysql server
-    require('database.php');
-
+    $result = $mysqli->query("SELECT * FROM orders WHERE user=?");
+    bind_param('i', $_SESSION['id']); // riesce a prendere l'id??
     
+    // $margin=0;
+    // while ($row = mysqli_fetch_array($result)) {
+    //     $counter = 0;
 
-    $result = $mysqli->query("SELECT * FROM $table WHERE user=?");
-    bind_param('s', $_SESSION['ID']); // riesce a prendere l'id??
-    
-    $margin=0;
-    while ($row = mysqli_fetch_array($result)) {
-        $counter = 0;
-
-        echo "<div class=profile_products style="."margin-top:".($margin*40)."%>";
+    //     echo "<div class=profile_products style="."margin-top:".($margin*40)."%>";
             
-            echo "<div class=profile_card>";
-                echo "<img src=".$row['img']." type=img>";
-                echo "<p>".$row['descr']."</p>";
-                echo "<button id=".$row['nameproduct']."-btncart name=".$row['nameproduct']." value=".$row['price']." type=submit> Order Now </button>";
-            echo "</div>";
+    //         echo "<div class=profile_card>";
+    //             echo "<img src=".$row['img']." type=img>";
+    //             echo "<p>".$row['descr']."</p>";
+    //             echo "<button id=".$row['nameproduct']."-btncart name=".$row['nameproduct']." value=".$row['price']." type=submit> Order Now </button>";
+    //         echo "</div>";
 
-            $row = mysqli_fetch_array($result);
-            if ($row == NULL) break;
-            echo "<div class=profile_divisor></div>";
+    //         $row = mysqli_fetch_array($result);
+    //         if ($row == NULL) break;
+    //         echo "<div class=profile_divisor></div>";
 
-            echo "<div class=profile_card>";
-                echo "<img src=".$row['img']." type=img>";
-                echo "<p>".$row['descr']."</p>";
-                echo "<button id=".$row['nameproduct']."-btncart name=".$row['nameproduct']." value=".$row['price']." type=submit> Order Now </button>";
-            echo "</div>";
+    //         echo "<div class=profile_card>";
+    //             echo "<img src=".$row['img']." type=img>";
+    //             echo "<p>".$row['descr']."</p>";
+    //             echo "<button id=".$row['nameproduct']."-btncart name=".$row['nameproduct']." value=".$row['price']." type=submit> Order Now </button>";
+    //         echo "</div>";
             
-            $row = mysqli_fetch_array($result);
-            if ($row == NULL) break;
+    //         $row = mysqli_fetch_array($result);
+    //         if ($row == NULL) break;
 
-            echo "<div class=profile_divisor></div>";
-                echo "<div class=profile_card>";
-                echo "<img src=".$row['img']." type=img>";
-                echo "<p>".$row['descr']."</p>";
-                echo "<button id=".$row['nameproduct']."-btncart name=".$row['nameproduct']." value=".$row['price']." type=submit> Order Now </button>";
-            echo "</div>";
+    //         echo "<div class=profile_divisor></div>";
+    //             echo "<div class=profile_card>";
+    //             echo "<img src=".$row['img']." type=img>";
+    //             echo "<p>".$row['descr']."</p>";
+    //             echo "<button id=".$row['nameproduct']."-btncart name=".$row['nameproduct']." value=".$row['price']." type=submit> Order Now </button>";
+    //         echo "</div>";
 
-        echo "</div>";
+    //     echo "</div>";
 
-        $margin++;
-    }
-
-
-
-    $result->close();
+    //     $margin++;
+    // }
+    // $result->close();
 ?> 
