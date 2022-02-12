56b969276e98c21e6ba98ad732bfc0c56e773435
Leonardo Gonfiantini <71825908+leonardogonfiantini@users.noreply.github.com>
2022-02-12 10:48:37 +0100

    Update database.php

--- db/database.php
+++ db/database.php
@@ -1,4 +1,4 @@
 <?php
   mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //error reporting for mysql server
-  $mysqli = mysqli_connect('localhost', 'S4825087', 'password', 'S4825087');
+  $mysqli = mysqli_connect('localhost', 'local', 'password', 'local');
 ?>
